dee5f556dc28e2efd58a260d4a0b89f492b2cc6f
ali <aliesmaeeli20@gmail.com>
2025-03-21 05:00:59 +0330

    update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -102,7 +102,7 @@ class UserController extends Controller
                 ]
             ],
             'eagerLoads' => [
-                'unitUsers'
+                'units'
             ]
         ];
 
